08f7d955e9c00d21647bafbf2946f98774cdca4a
Madhu Sudhan <2300031210@kluniversity.in>
2026-03-28 20:07:14 +0530

    Greedy Problems

---
+++ Greedy Algorithms/Leetcode 53 - Maximum Subarray.cpp
@@ -0,0 +1,17 @@
+// Problem Link: https://leetcode.com/problems/maximum-subarray/description/
+// Time Complexity: O(n)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++){
+            curr = max(nums[i], curr + nums[i]);
+            maxSum = max(maxSum, curr);
+        }
+
+        return maxSum;
+    }
+};
