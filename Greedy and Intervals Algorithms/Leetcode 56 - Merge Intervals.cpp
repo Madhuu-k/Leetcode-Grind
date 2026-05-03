e155d5a2ab920dd6a4105825915919f44b707d2b
Madhu Sudhan <2300031210@kluniversity.in>
2026-05-03 20:11:24 +0530

    Greedy-Intervals Problems

---
+++ Greedy and Intervals Algorithms/Leetcode 56 - Merge Intervals.cpp
@@ -0,0 +1,23 @@
+// Problem Link: https://leetcode.com/problems/merge-intervals
+// Time Complexity: O(nlogn)
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+
+        for(auto it : intervals){
+            if(res.empty()) res.push_back(it);
+
+            vector<int> last = res.back();
+            if(it[0] <= last[1]){
+                res.back()[1] = max(res.back()[1], it[1]);
+            }
+
+            else res.push_back(it);
+        }
+
+        return res;
+    }
+};
