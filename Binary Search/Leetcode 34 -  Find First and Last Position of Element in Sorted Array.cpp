065e249242e6733932ab3d7747cecc914944e24b
Madhu Sudhan <2300031210@kluniversity.in>
2026-06-16 18:20:02 +0530

    Binary Search

--- Binary Search/Leetcode 34 -  Find First and Last Position of Element in Sorted Array.cpp
+++ Binary Search/Leetcode 34 -  Find First and Last Position of Element in Sorted Array.cpp
@@ -1,18 +1,37 @@
 // Problem Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/1696171501/?envType=problem-list-v2&envId=array
-// Time Complexity: O(n)
+// Time Complexity: O(log n)
 
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1, last = -1;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target) {
-                if(first == -1) first = i;
-                last = i;
+    int findLeft(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1, ans = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                ans = mid;
+                right = mid - 1;
             }
+            else if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
         }
+        return ans;
+    }
 
-        return {first, last};
+    int findRight(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1, ans = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                ans = mid;
+                left = mid + 1;
+            }
+            else if(nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findLeft(nums, target), findRight(nums, target)};
     }
 };
-
--- Binary Search/Leetcode 74 - Search a 2D Matrix.cpp
+++ Binary Search/Leetcode 74 - Search a 2D Matrix.cpp
@@ -1,18 +1,23 @@
 // Problem Link: https://leetcode.com/problems/search-a-2d-matrix/
-// Time Complexity: o(log(m * n))
+// Time Complexity: o(m. log n)
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        int left = 0, right = m * n - 1;
+    bool isThere(vector<int>& arr, int target){
+        int left = 0, right = arr.size() - 1;
+        bool res = false;
         while(left <= right){
-            int mid = left  + (right - left) / 2;
-            int row = mid / n; int col = mid % n;
-            int midVal = matrix[row][col];
-            if(midVal == target) return true;
-            if(midVal < target) left = mid + 1;
-            else right = mid - 1;
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target) return true;
+            else if(arr[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(auto it : matrix){
+            if(isThere(it, target)) return true;
         }
         return false;
     }
